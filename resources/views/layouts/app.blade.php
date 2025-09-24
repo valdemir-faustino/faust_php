41d266a7e142ab3a46b34020177c44963b48b71a
Valdemir-Faustino <valfaustino2@hotmail.com>
2025-09-23 22:16:20 -0300

    Atualização de view show e do layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,42 +1,49 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>@yield('title', 'Meu Sistema')</title>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-        crossorigin="anonymous">
-    </script>
+    
+    @stack('styles')
 </head>
-
 <body>
-    <nav class="navbar navbar-expand-lg bg-info">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-success">
         <div class="container-fluid">
+            <a class="navbar-brand" href="{{ route('index') }}">MeuApp</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
                 aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+            
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('index')}}">Home</a>
+                        <a class="nav-link @if (request()->routeIs('index')) active @endif" href="{{ route('index') }}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('create')}}">Novo elemento</a>
+                        <a class="nav-link @if (request()->routeIs('create')) active @endif" href="{{ route('create') }}">Novo elemento</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
+                        <a class="nav-link" href="">Outro link</a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
-    @yield('content')
-</body>
 
+    <main class="container mt-4">
+        @yield('content')
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
+
+    @stack('scripts')
+</body>
 </html>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,14 +1,27 @@
 @extends('layouts.app')
+
 @section('content')
-
-    <h1>Detalhe</h1>
     <x-alert />
-      @if (isset($cad))
 
-      Id : {{$cad->id}}<br>
-      Email : {{$cad->email}}
-          
-      @endif
-    
+    @isset($cad)
+        <div class="container mt-4 d-flex justify-content-center">
+            <div class="card border-success shadow-sm" style="max-width: 30rem;">
+                <div class="card-header bg-success text-white">
+                    <h4 class="mb-0">Detalhes do Cadastro</h4>
+                </div>
+                <div class="card-body">
+                    <dl class="row mb-0">
+                        <dt class="col-sm-4">ID</dt>
+                        <dd class="col-sm-8">{{ $cad->id }}</dd>
 
+                        <dt class="col-sm-4">Email</dt>
+                        <dd class="col-sm-8">{{ $cad->email }}</dd>
+                    </dl>
+                </div>
+                <div class="card-footer text-end bg-light">
+                    <a href="{{ route('index') }}" class="btn btn-outline-success btn-sm">Voltar</a>
+                </div>
+            </div>
+        </div>
+    @endisset
 @endsection
